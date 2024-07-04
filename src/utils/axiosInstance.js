e9dcbc5800da1632ab748f1c31935b150afef1eb
Neel Gujarati <patelneel0767@gmail.com>
2024-07-04 16:33:43 -0400

    Fixing Env variables

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: 'http://13.58.166.104:3000',
   headers: {
     'Content-Type': 'application/json',
   },
